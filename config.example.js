cf03215b07cc713e1c00d80055588930b6ea71f5
Christina Gardner-McCune <cgardnermccune@gmail.com>
2018-10-01 14:09:03 -0400

    updated assignment #3 - Initial Commit

---
+++ config.example.js
@@ -0,0 +1,10 @@
+//This file holds any configuration variables we may need 
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+//You can refer to this uri to connect to your database in other files config.db.uri
+
+module.exports = {
+  db: {
+    uri: '', //place the URI of your mongo database here.
+  }
+};
